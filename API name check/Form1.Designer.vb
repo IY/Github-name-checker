73c9f086a3d0b0a303a659d131467d89301d35f3
Analysis <25322999+Analysis@users.noreply.github.com>
2018-07-13 21:00:16 -0600

    Add files via upload

---
+++ API name check/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.TextBox1.Location = New System.Drawing.Point(187, 6)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(133, 405)
+        Me.TextBox1.TabIndex = 99
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(297, 457)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(60, 43)
+        Me.ListBox1.TabIndex = 1
+        Me.ListBox1.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(-2, 416)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(333, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 214)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 32)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(43, 214)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 32)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(8, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(172, 38)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Load"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(8, 56)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(172, 38)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Export"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 18)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Loaded: 0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(58, 174)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 18)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Good: 0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(58, 192)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 18)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Taken: 0"
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(8, 263)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(172, 147)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 247)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Raw Response:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(3, 107)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.TabIndex = 100
+        Me.Label5.Text = "Username:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 133)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 13)
+        Me.Label6.TabIndex = 101
+        Me.Label6.Text = "Token:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(59, 104)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(119, 20)
+        Me.TextBox3.TabIndex = 102
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(59, 130)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox4.Size = New System.Drawing.Size(119, 20)
+        Me.TextBox4.TabIndex = 103
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 439)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Ratz Github Name Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ API name check/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Text
+Imports System.IO
+Imports System.Net
+Imports System.Web
+
+Public Class Form1
+
+    Dim logincookie As CookieContainer
+    Dim good, bad As Integer
+    Dim pencil As Boolean = False
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button2.Show()
+        Button1.Hide()
+        pencil = False
+        ProgressBar1.Value = 0
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        OpenFileDialog1.Filter = "Text files|*.txt"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            ListBox1.Items.AddRange(File.ReadAllLines(OpenFileDialog1.FileName))
+            Label1.Text = "Loaded: " & ListBox1.Items.Count.ToString
+            ProgressBar1.Value = 0
+            ProgressBar1.Maximum = ListBox1.Items.Count - 1
+            ListBox1.SelectedIndex = 0
+        End If
+    End Sub
+    Sub HelloMumPipl(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        UpdateTextBox(e.Data)
+    End Sub
+    Private Delegate Sub UpdateTextBoxDelegate(ByVal Text As String)
+    Private Sub UpdateTextBox(ByVal Tex As String)
+        If Me.InvokeRequired Then
+            Dim del As New UpdateTextBoxDelegate(AddressOf UpdateTextBox)
+            Dim args As Object() = {Tex}
+            Me.Invoke(del, args)
+        Else
+            TextBox2.Text &= Tex & Environment.NewLine
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Do While pencil = False
+            If pencil = False Then
+goagain:
+                Dim currentaccount As String = ListBox1.SelectedItem.ToString
+                Try
+                    ListBox1.SelectedIndex += 1
+                Catch ex As Exception
+                    MsgBox("Done")
+                    Exit Sub
+                End Try
+
+
+                TextBox2.Clear()
+
+                Dim p As New Process
+                p.StartInfo.FileName = "curl.exe"
+
+                p.StartInfo.Arguments = "C:\Windows\System32\curl.exe -u " & TextBox3.Text & ":" & TextBox4.Text & " https://api.github.com/users/" & currentaccount
+
+
+                p.StartInfo.UseShellExecute = False
+                p.StartInfo.RedirectStandardOutput = True
+                p.StartInfo.CreateNoWindow = True
+                AddHandler p.OutputDataReceived, AddressOf HelloMumPipl
+                p.Start()
+                p.BeginOutputReadLine()
+
+                p.WaitForExit()
+
+                If TextBox2.Text.ToString.Contains("API rate limit exceeded") Then
+                    MsgBox("Rate limited!")
+                    Exit Sub
+
+                Else
+                    If TextBox2.Text.ToString.Contains("Not Found") Then
+                    good += 1
+                    Label2.Text = "Good: " & good.ToString
+
+                    If pencil = False Then
+                        TextBox1.Text += currentaccount & vbNewLine
+                        Me.Refresh()
+                    End If
+                Else
+                    bad += 1
+                    Label3.Text = "Bad: " & bad.ToString
+                End If
+
+                Try
+                    ProgressBar1.Value += 1
+                Catch ex As Exception
+                End Try
+
+                If pencil = False Then
+                    GoTo goagain
+                End If
+            End If
+            End If
+        Loop
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button2.Hide()
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        pencil = True
+        BackgroundWorker1.CancelAsync()
+        MsgBox("Stopped")
+    End Sub
+
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        SaveFileDialog1.Filter = "text files|*.txt"
+        If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            File.WriteAllText(OpenFileDialog1.FileName, TextBox1.Text)
+        End If
+    End Sub
+End Class
